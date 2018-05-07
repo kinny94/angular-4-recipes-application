df339660b431b9591a825971438a468cbd8bd36a
kinny94 <ad.sam007@gmail.com>
2018-05-07 17:07:01 -0400

    Added recipe list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+	selector: 'app-recipe-list',
+	templateUrl: './recipe-list.component.html',
+	styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+	
+	recipes: Recipe[] = [
+		new Recipe( 'A Test recipe ', 'This is a test', 'http://www.clubnoor.com/uploads/recipes/7e20d8583a374b1490b859b8223f373e.png' )
+	];
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	constructor() { }
+	
+	ngOnInit() {
+	}
+	
 }
