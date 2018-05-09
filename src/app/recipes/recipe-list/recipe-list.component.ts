a04f0682cb7a5e6217890eedb1986673a6135cc0
kinny94 <ad.sam007@gmail.com>
2018-05-09 17:28:40 -0400

    ]Passed data with event and property binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,8 +8,10 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 	
+	@Output() recipeWasSelected = new EventEmitter<Recipe>();
 	recipes: Recipe[] = [
-		new Recipe( 'A Test recipe ', 'This is a test', 'http://www.clubnoor.com/uploads/recipes/7e20d8583a374b1490b859b8223f373e.png' )
+		new Recipe( 'A Test recipe ', 'This is a test', 'http://www.clubnoor.com/uploads/recipes/7e20d8583a374b1490b859b8223f373e.png' ),
+		new Recipe( 'Another Test recipe ', 'This is the second test', 'http://www.clubnoor.com/uploads/recipes/7e20d8583a374b1490b859b8223f373e.png' )
 	];
 
 	constructor() { }
@@ -17,4 +19,7 @@ export class RecipeListComponent implements OnInit {
 	ngOnInit() {
 	}
 	
+	onRecipeSelected( recipe: Recipe ){
+		this.recipeWasSelected.emit( recipe );
+	}
 }
